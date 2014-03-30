64261b78fbf8cdce4f9e1265c99745caa6bc5654
demslam <briggs.m@gmail.com>
2014-03-30 11:13:42 -0700

    test... scripts created by me

---
+++ wallop.sh
@@ -0,0 +1,21 @@
+#/bin/bash
+###
+#Bash script to download/compile Wallop
+#Date: 2014/03/30
+#By: Matt Briggs
+#For: Ubuntu 12.04
+####
+
+#directory
+dir=~
+
+#ruby
+sudo apt-get remove ruby1.8
+sudo apt-get install ruby1.9.1 ruby1.9.1-dev ruby1.9.1-full rubygems
+sudo gem install bundle
+
+#git
+cd $dir
+sudo apt-get install git
+git clone https://github.com/maddox/wallop
+cd wallop
