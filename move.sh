18533e306545f5830552ff07a29e73c0d558dd37
demslam <briggs.m@gmail.com>
2018-08-25 14:37:29 -0700

    Create move.sh

---
+++ move.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+####
+#By: Matt (Dem) Briggs
+#Date: 2018/07/04
+#Description: movies (copy) files from one folder to another and deletes unwanted file types
+####
+
+#source folder
+sour=/mnt/500/downloads/movie/
+
+#destination folder
+dest=/media/47d6f2e9-2c91-440e-bfd7-27598d72b3cd/upload/movie/
+
+#add file extension to delete
+delete=( *.par2 *.txt *.nfo *.nzb *sample* )
+
+#rsync copy files over
+rsync -ah --progress $sour $dest
+
+#delete unwated files in the copied directory
+for i in "${delete[@]}"
+do
+  :
+  find $dest -type f -name $i -delete
+done
