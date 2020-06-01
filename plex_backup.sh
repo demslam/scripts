f46caa35e4095e59b9c17b5e44c99c8d3d5c64e5
demslam <briggs.m@gmail.com>
2020-06-01 13:58:23 -0700

    Update plex_backup.sh

--- plex_backup.sh
+++ plex_backup.sh
@@ -1,1 +1,16 @@
+#/bin/bash
 
+####
+#By: Matt (Dem) Briggs
+#Date: 2020/06/01
+#Backup plex server items and Tautulli items
+####
+
+plexsrc='/var/lib/plexmediaserver/Library/Application Support/Plex Media Server'
+dest='#to location'
+cmd='tar -czvf plex-backup.tar.gz $plexsrc'
+
+echo "Plex backup and restore script"
+echo -n "Backup or restore?"
+read opt
+echo "$opt selected"
