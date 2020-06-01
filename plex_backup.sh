0f08b131bd520284bbd6ff915efb3fb780342fb3
demslam <briggs.m@gmail.com>
2020-06-01 14:40:26 -0700

    Update plex_backup.sh

--- plex_backup.sh
+++ plex_backup.sh
@@ -1,5 +1,4 @@
 #/bin/bash
-
 ####
 #By: Matt (Dem) Briggs
 #Date: 2020/06/01
@@ -7,10 +6,33 @@
 ####
 
 plexsrc='/var/lib/plexmediaserver/Library/Application Support/Plex Media Server'
+tautsrc='/opt/Tautulli'
 dest='#to location'
-cmd='tar -czvf plex-backup.tar.gz $plexsrc'
+d=$(date +%Y%m%d%H%M%S)
+d1="date"
+cmd=$(tar -czvf plex-backup-$d.tar.gz $plexsrc')
 
 echo "Plex backup and restore script"
 echo -n "Backup or restore?"
 read opt
 echo "$opt selected"
+echo $cmd1
+
+#!/bin/bash
+
+####
+#By: Matt (Dem) Briggs Date: 2020/06/01 Backup plex server items and Tautulli items
+####
+plexsrc='/var/lib/plexmediaserver/Library/Application Support/Plex Media Server'
+tautsrc='/opt/Tautulli'
+dest='#to location'
+d=$(date +%Y%m%d%H%M%S)
+d1="date"
+cmd=$(tar -czvf plex-backup-$d.tar.gz $plexsrc')
+cmd1=$(touch plex-backup-$d.tar.gz)
+echo "Plex backup and restore script"
+echo
+#read opt
+#echo "$opt selected"
+$cmd
+
